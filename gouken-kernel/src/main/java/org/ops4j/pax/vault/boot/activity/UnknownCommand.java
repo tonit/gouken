dc5467b786e56af67d7409fdb420a32cf380b7c4
Toni Menzel <toni@okidokiteam.com>
2010-03-18 09:02:29 +0100

    refactored modules

---
+++ gouken-kernel/src/main/java/org/ops4j/pax/vault/boot/activity/UnknownCommand.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2009 Toni Menzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.vault.boot.activity;
+
+import java.util.Map;
+import org.ops4j.pax.vault.boot.Command;
+
+/**
+ * @author Toni Menzel
+ * @since Mar 4, 2010
+ */
+public class UnknownCommand implements Command
+{
+
+    public UnknownCommand( Map<String, String> map )
+    {
+    }
+
+    public void execute()
+    {
+        throw new RuntimeException( "Unknown command!" );
+    }
+}
