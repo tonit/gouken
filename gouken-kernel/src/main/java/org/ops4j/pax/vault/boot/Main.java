d9f30bbfe48b2b703c61afe678042563eda49314
Toni Menzel <toni@okidokiteam.com>
2010-03-18 09:21:59 +0100

    split out command processing.

--- gouken-kernel/src/main/java/org/ops4j/pax/vault/boot/Main.java
+++ gouken-kernel/src/main/java/org/ops4j/pax/vault/boot/Main.java
@@ -36,71 +36,11 @@ import org.ops4j.pax.vault.boot.activity.UnknownCommand;
 public class Main
 {
 
-    private static Log LOG = LogFactory.getLog( Main.class );
-
     public static void main( String[] args )
         throws Exception
     {
-        new Main().mapCommand( args ).execute();
+        new CommandController().mapCommand( args ).execute();
     }
 
-    /**
-     * Usually you want to make a framework:
-     * 1. run
-     * 2. shutdown
-     * 3. re-start
-     *
-     * @param args commandline arguments
-     *
-     * @return a command
-     */
-    public Command mapCommand( String[] args )
-    {
-        Map<String, String> map = new HashMap<String, String>();
-        mapArgs( args, map );
-        if( map.containsKey( "start" ) )
-        {
-            return new StartCommand( map );
-        }
-        else if( map.containsKey( "stop" ) )
-        {
-            return new StopCommand();
 
-        }
-        else if( map.containsKey( "restart" ) )
-        {
-            return new RestartCommand( new StartCommand( map ), new StopCommand() );
-
-        }
-        else if( map.containsKey( "status" ) )
-        {
-            return new StatusCommand( map );
-        }
-        else
-        {
-            return new UnknownCommand( map );
-        }
-    }
-
-    private static void mapArgs( String[] args, Map<String, String> map )
-    {
-        for( String s : args )
-        {
-            if( s != null && s.length() > 0 )
-            {
-                int eq = s.indexOf( "=" );
-                if( eq < 0 )
-                {
-                    map.put( s, "true" );
-                }
-                else
-                {
-                    String key = s.substring( 0, eq );
-                    String value = s.substring( eq + 1 );
-                    map.put( key, value );
-
-                }
-            }
-        }
-    }
 }
